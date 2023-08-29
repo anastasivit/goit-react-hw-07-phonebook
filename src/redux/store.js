25806bf5012bb107cb16fedca8a5d055f6cc43ed
anastasivit <119945000+anastasivit@users.noreply.github.com>
2023-08-29 21:17:27 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['filter'],
+  blacklist: ['contacts.isLoading', 'contacts.error'],
 };
 
 const persistedReducer = persistReducer(persistConfig, contactsReducer);
